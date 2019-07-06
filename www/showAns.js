2b41f8f11fb65d08d274ec6f53abcbfc10e5ab0c
booude <booude@gmail.com>
2019-07-06 00:27:29 -0300

    Mudança de nome de um arquivo .js ainda não utilizado

--- www/showAns.js
+++
@@ -1,9 +0,0 @@
-var showAns = document.getElementById("showAns");
-var n10000 = 0;
-function add(value) {
-    var sum = n10000 + value;
-    if (sum > -1 && sum < 100) {
-        n10000 = sum;
-    }
-    showAns.innerHTML = n10000;
-}
